3188bd9d66be6fca6910174814cc8c7d2ef1ca77
OlmoVisedo <visedoolmo@gmail.com>
2015-02-25 18:40:59 +0100

    Corrección de movimiento
    Arreglado el movimiento del 'personaje', mejoras del nivel

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour {
 	
 	public float speed = 2f;
 	public Vector2 maxVelocity = new Vector2(2,3);
-	public float velocidad = 5f;
+	public float velocidad = 2f;
 	//public Light linterna;
 
 	// Use this for initialization
@@ -19,20 +19,19 @@ public class Player : MonoBehaviour {
 	void Update () {
 		
 		var absVelX = Mathf.Abs (rigidbody.velocity.x);
-		var forceX = 0f;
-		var forceY = 0f;
+
 		
 		// Calculate Velocity X
 		if (Input.GetKey("right"))
 		{
 			
 			//Esto lo que hace es frenar cuando voy a la izquierda y pulso a la derecha
-			if(rigidbody.velocity.x <= 0){
+			if(absVelX < maxVelocity.x){
 				//rigidbody.AddForce (new Vector3 (velocidad, 0, 0), ForceMode.Impulse);
-				rigidbody.velocity = new Vector3(rigidbody.velocity.x+velocidad, 0,0);
+				rigidbody.velocity = new Vector3(velocidad, rigidbody.velocity.y,0);
 			}
-			if(absVelX < maxVelocity.x)
-				forceX = speed;
+
+				
 			
 			//Esto pone el sprite mirando a la derecha
 			this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
@@ -41,14 +40,14 @@ public class Player : MonoBehaviour {
 		else if (Input.GetKey("left"))
 		{
 			//Esto lo que hace es frenar cuando voy a la derecha y pulso a la izquierda
-			if(rigidbody.velocity.x >= 0)
-				//rigidbody.AddForce (new Vector3 (-velocidad, 0, 0), ForceMode.Impulse);
-				rigidbody.velocity = new Vector3(rigidbody.velocity.x-velocidad, 0,0);
-			
 			if (absVelX < maxVelocity.x)
-				forceX = -speed;
+				//rigidbody.AddForce (new Vector3 (-velocidad, 0, 0), ForceMode.Impulse);
+				rigidbody.velocity = new Vector3(-velocidad, rigidbody.velocity.y,0);
+			
+
+				
 			//Esto pone el sprite mirando a la izquierda
-			this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+			this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
 			
 		}
 
