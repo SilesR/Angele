e5ac97b3fe6d437cbb20e10b0a67f00fb0f1e4df
OlmoVisedo <visedoolmo@gmail.com>
2015-02-26 19:24:55 +0100

    Mejora Nivel_01
    Muerte al caer por precipicio.
    Comienzo en la creación de los colliders del liquido.

---
+++ Assets/Scripts/Disparador.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparador : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+		animator = GetComponent<Animator>();
+		animator.SetBool("DisparadorLiquido", false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider target){
+
+		animator.SetBool("DisparadorLiquido", true);
+	}
+}
---
+++ Assets/Scripts/Muerte.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Muerte : MonoBehaviour {
+	public string nameLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider target){
+		if (target.transform.tag == "Player") {
+			Application.LoadLevel(nameLevel);
+		}
+	}
+}
