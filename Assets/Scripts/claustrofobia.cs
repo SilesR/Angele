010f70b35ed460335f1806839d06d8b54d6a32b1
RocioB <rociobarlo@gmail.com>
2015-03-04 18:48:18 +0100

    Intento de Script claustrofobia

---
+++ Assets/Scripts/claustrofobia.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class claustrofobia : MonoBehaviour {
+
+	Camera camara;
+	public int factor_miedo = 1; // El factor muiedo indicará la velocidad con la que se reduce el campo de visión
+	public int distancia = 1;
+	// Use this for initialization
+
+	void Start () {
+		//Obtenemos el componente cámara del objeto en el que está insertado el script
+		camara = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camara.fieldOfView = camera.fieldOfView - Time.deltaTime*factor_miedo;
+
+
+
+			transform.position.z = new Vector3(0,0,transform.position.z - Time.deltaTime*distancia);
+
+	}
+}
