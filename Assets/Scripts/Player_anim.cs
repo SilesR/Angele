3917e4bf63ea7c6bf57c80961736a52bc8f1fd0f
ArbolPotted <teresahernandezpizarro@gmail.com>
2015-03-04 17:46:58 +0100

    Angele izq-der

--- Assets/Scripts/Player_anim.cs
+++ Assets/Scripts/Player_anim.cs
@@ -6,7 +6,7 @@ public class Player_anim : MonoBehaviour {
 	public float speed = 2f;
 	public Vector3 maxVelocity = new Vector3(2,3,3);
 	public float velocidad = 2f;
-
+	float isRight;
 
 	Animator anim;
 
@@ -14,7 +14,7 @@ public class Player_anim : MonoBehaviour {
 	void Start () {
 
 		anim = GetComponent<Animator>();
-
+		isRight = this.transform.localScale.x;
 		//if (ontriggerenter
 		
 	}
@@ -27,7 +27,7 @@ public class Player_anim : MonoBehaviour {
 		
 		// Calculate Velocity X
 		if (Input.GetKey("right")){
-			
+
 			//Esto lo que hace es frenar cuando voy a la izquierda y pulso a la derecha
 			if(absVelX < maxVelocity.x){
 				//rigidbody.AddForce (new Vector3 (velocidad, 0, 0), ForceMode.Impulse);
@@ -37,13 +37,14 @@ public class Player_anim : MonoBehaviour {
 				anim.SetBool ("correr", true); 
 
 				//Esto pone el sprite mirando a la derecha
-				this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+				this.transform.localScale = new Vector3(isRight, this.transform.localScale.y, this.transform.localScale.z);
 
 			}else{
 				//anim.SetBool ("correr", false); acitvar este o desactivar los dos no hace anim bien
 			}				
 			
 		}else if (Input.GetKey("left")){
+
 			//Esto lo que hace es frenar cuando voy a la derecha y pulso a la izquierda
 			if (absVelX < maxVelocity.x)
 				//rigidbody.AddForce (new Vector3 (-velocidad, 0, 0), ForceMode.Impulse);
@@ -52,7 +53,7 @@ public class Player_anim : MonoBehaviour {
 			anim.SetBool ("correr", true);
 
 			//Esto pone el sprite mirando a la izquierda
-			this.transform.localScale = new Vector3(this.transform.localScale.x*-1, this.transform.localScale.y, this.transform.localScale.z);
+			this.transform.localScale = new Vector3(-isRight, this.transform.localScale.y, this.transform.localScale.z);
 		}else{
 			anim.SetBool ("correr", false);
 		}
