0e29c080fb47ad08be6dae23afe8f99aafaa7d8e
SilesR <rafasiles@gmail.com>
2015-03-05 18:39:11 +0100

    #508 Crear Fin de niveles

---
+++ Assets/Fin_Nivel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fin_Nivel : MonoBehaviour {
+	public string nameLevel;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider target){
+		if (target.transform.tag == "Player") {
+			Application.LoadLevel(nameLevel);
+		}
+	}
+
+
+}
